99c9ba4d39275e08f76a1f2d7f88b2c20668e242
Khương Lê Bá <khuonghigo@gmail.com>
2021-08-09 11:06:17 +0700

    Add project files.

---
+++ TinhTienNhaKhoa/TinhTienNhaKhoa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TinhTienNhaKhoa
+{
+    public partial class TinhTienNhaKhoa : Form
+    {
+        public TinhTienNhaKhoa()
+        {
+            InitializeComponent();
+        }
+
+        private void TinhTienNhaKhoa_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tinhTien(object sender, EventArgs e)
+        {
+            Button tinhTien = new Button();
+        }
+    }
+}
